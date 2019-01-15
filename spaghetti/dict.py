debadf6b86fbd5f76eb1b29148ce0a5e4da5c9ef
Ralph Tang <r33tang@uwaterloo.ca>
2019-01-15 12:11:52 -0500

    Fix tab convention

--- spaghetti/dict.py
+++ spaghetti/dict.py
@@ -1,17 +1,18 @@
 def flattened(obj):
-  def merge(d1, d2):
-    d = d1.copy()
-    for k, v in d2.items():
-      d[k].extend(v)
-    return d
-  table = defaultdict(list)
-  if isinstance(obj, dict):
-    for k, v in obj.items():
-      if isinstance(v, dict) or isinstance(v, list) or isinstance(v, tuple):
-        table = merge(table, flattened(v))
-      else:
-        table[k].append(v)
-  elif isinstance(obj, list) or isinstance(obj, tuple):
-    for x in obj:
-      table = merge(table, flattened(x))
-  return table
+    def merge(d1, d2):
+        d = d1.copy()
+        for k, v in d2.items():
+            d[k].extend(v)
+        return d
+
+    table = defaultdict(list)
+    if isinstance(obj, dict):
+        for k, v in obj.items():
+            if isinstance(v, dict) or isinstance(v, list) or isinstance(v, tuple):
+                table = merge(table, flattened(v))
+            else:
+                table[k].append(v)
+    elif isinstance(obj, list) or isinstance(obj, tuple):
+        for x in obj:
+            table = merge(table, flattened(x))
+    return table
