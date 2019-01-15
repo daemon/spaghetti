59b44b7ff64686f5c3fecb5a15a3a297612b7ada
Ralph Tang <r33tang@uwaterloo.ca>
2019-01-15 11:58:33 -0500

    Fix bug

--- spaghetti/dict.py
+++ spaghetti/dict.py
@@ -8,10 +8,10 @@ def flattened(obj):
   if isinstance(obj, dict):
     for k, v in obj.items():
       if isinstance(v, dict) or isinstance(v, list) or isinstance(v, tuple):
-        table = merge(table, flatten(v))
+        table = merge(table, flattened(v))
       else:
         table[k].append(v)
   elif isinstance(obj, list) or isinstance(obj, tuple):
     for x in obj:
-      table = merge(table, flatten(x))
+      table = merge(table, flattened(x))
   return table
