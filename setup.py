08aab3c75993b61c43c5387a960cf4f42b440ca3
Ralph Tang <r33tang@uwaterloo.ca>
2019-01-15 11:56:28 -0500

    Add setup file

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+
+setuptools.setup(
+    name="spaghetti",
+    version="0.0.1",
+    author="Ralph Tang",
+    author_email="r33tang@uwaterloo.ca",
+    description="A whole mess of iterable utilities.",
+    url="https://github.com/daemon/spaghetti",
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+)
